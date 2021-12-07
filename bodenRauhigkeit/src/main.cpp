4c38a29eb8db972535c7a7ab4ad86f8064dea6fd
Julius Preuschoff <julius.preuschoff@haw-hamburg.de>
2021-12-07 15:22:45 +0100

    static für rounds

--- bodenRauhigkeit/src/main.cpp
+++ bodenRauhigkeit/src/main.cpp
@@ -4,80 +4,30 @@
 #define LENKUNG_PWM_PIN 9
 #define ESC_PWM_PIN 13
 
-
 #include <ros.h>
 #include <std_msgs/Int16.h>
-#include <std_msgs/Float32.h>
-#include <std_msgs/Float32MultiArray.h>
 
-#include "IMU.h" // functions for the IMU
 #include "rounds.h"
-#include "servoControl.h"
+
 
 ros::NodeHandle  nh;
-
-// IMU Data
-std_msgs::Float32MultiArray imu_msg;
-ros::Publisher pub_imu("IMU", &imu_msg);
 
 // Umdrehungs Data
 std_msgs::Int16 rounds_msg;
 ros::Publisher pub_rounds("rounds", &rounds_msg);
 
-// Lenkungs Servo PWM
-ros::Subscriber<std_msgs::UInt16> sub_lenkung("lenkung", setLenkwinkel);
-
-// ESC PWM
-ros::Subscriber<std_msgs::UInt16> sub_speed("speed", setSpeed);
-
 
 void setup() {
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-  Wire.begin();
-
-  nh.initNode();
-  nh.advertise(pub_imu);
+  
   nh.advertise(pub_rounds);
-
-  nh.subscribe(sub_lenkung);
-  lenkung.attach(LENKUNG_PWM_PIN);
-
-  nh.subscribe(sub_speed);
-  ESC.attach(ESC_PWM_PIN);
-
-  imu_msg.data_length = 9; // 9 Elemente
-  imu_msg.data = (float*)malloc(sizeof(float) * imu_msg.data_length);
 
   rounds_msg.data = 0;
 }
 
 void loop() 
 {   
-
-	// IMU:	
-	getAccel_Data();
-	getGyro_Data();
-	getCompassDate_calibrated(); // compass data has been calibrated here 
-	getHeading();				//before we use this function we should run 'getCompassDate_calibrated()' frist, so that we can get calibrated data ,then we can get correct angle .					
-	getTiltHeading();           
-	delay(300); // ACHTUNG KANN PROBLEME MACHEN !!!
-
-	// rounds:
 	rounds_msg.data = getRounds();
-
-    imu_msg.data[0] = Axyz[0];
-    imu_msg.data[1] = Axyz[1];
-    imu_msg.data[2] = Axyz[2];
-
-    imu_msg.data[3] = Gxyz[0];
-    imu_msg.data[4] = Gxyz[1];
-    imu_msg.data[5] = Gxyz[2];
-
-    imu_msg.data[6] = Mxyz[0];
-    imu_msg.data[7] = Mxyz[1];
-    imu_msg.data[8] = Mxyz[2];
-
-    pub_imu.publish(&imu_msg);
 	pub_rounds.publish(&rounds_msg);
     nh.spinOnce();
+	delay(300);
 }

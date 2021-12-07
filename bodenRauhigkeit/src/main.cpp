c3edaece1fea90bbdd255aca4c4f67fb7573856e
Julius Preuschoff <julius.preuschoff@haw-hamburg.de>
2021-12-07 15:38:53 +0100

    IMU überträgt daten, daten ändern sich nicht

--- bodenRauhigkeit/src/main.cpp
+++ bodenRauhigkeit/src/main.cpp
@@ -6,7 +6,10 @@
 
 #include <ros.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Float32MultiArray.h>
 
+#include "IMU.h" // functions for the IMU
 #include "rounds.h"
 
 
@@ -16,18 +19,40 @@ ros::NodeHandle  nh;
 std_msgs::Int16 rounds_msg;
 ros::Publisher pub_rounds("rounds", &rounds_msg);
 
+// IMU Data
+std_msgs::Float32MultiArray imu_msg;
+ros::Publisher pub_imu("IMU", &imu_msg);
+
+
+
 
 void setup() {
-  
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Wire.begin();
+
+  nh.advertise(pub_imu);
   nh.advertise(pub_rounds);
 
   rounds_msg.data = 0;
+
+  imu_msg.data_length = 9; // 9 Elemente
+  imu_msg.data = (float*)malloc(sizeof(float) * imu_msg.data_length);
 }
 
 void loop() 
 {   
+	// IMU:	
+	getAccel_Data();
+	getGyro_Data();
+	getCompassDate_calibrated(); // compass data has been calibrated here 
+	getHeading();				//before we use this function we should run 'getCompassDate_calibrated()' frist, so that we can get calibrated data ,then we can get correct angle .					
+	getTiltHeading();
+
 	rounds_msg.data = getRounds();
+
 	pub_rounds.publish(&rounds_msg);
-    nh.spinOnce();
+    pub_imu.publish(&imu_msg);
+
+	nh.spinOnce();
 	delay(300);
 }

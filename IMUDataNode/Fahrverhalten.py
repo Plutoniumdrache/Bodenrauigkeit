f97db8593828346d76314230093048cdfdf50fe7
Julius Preuschoff <julius.preuschoff@haw-hamburg.de>
2021-12-22 14:54:51 +0100

    pythonnodes

---
+++ IMUDataNode/Fahrverhalten.py
@@ -0,0 +1,24 @@
+import rospy
+import std_msgs
+
+def Fahrverhalten():
+    pubSpeed = rospy.Publisher('speed', int)
+    pubAngle = rospy.Publisher('angle', int)
+    rospy.init_node('speed')
+    rospy.init_node('angle')
+    rate = rospy.Rate(10)
+    speed = 0
+    angle = 0
+    while not rospy.is_shutdown():
+        rospy.loginfo(speed)
+        rospy.loginfo(angle)
+        pubSpeed.publish(speed)
+        pubSpeed.publish(angle)
+        rate.sleep
+
+
+if __name__ == '__main__':
+    try:
+        Fahrverhalten()
+    except rospy.ROSInterruptException:
+        pass

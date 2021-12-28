1c97a2690afb931fc729c64b5ec1a608ba4f0087
Julius Preuschoff <julius.preuschoff@haw-hamburg.de>
2021-12-28 12:52:28 +0100

    first try

---
+++ rosNodes/IMUsend_node.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+ROS node for collecting and sending the data from the IMU on the topic IMU
+"""
+
+from mpu9250_i2c import *
+import rospy
+from std_msgs.msg import Float32MultiArray
+
+# sleep in here not good
+time.sleep(1) # delay necessary to allow mpu9250 to settle
+
+def collectData():
+    pub = rospy.Publisher('IMU', Float32MultiArray)
+    rospy.init_node('IMU')
+    rate = rospy.Rate(10)
+    accelData = Float32MultiArray()
+        
+    while not rospy.is_shutdown():
+        try:
+            ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+        except:
+            continue
+        
+        accelData = [ax, ay, az]
+        
+        rospy.loginfo("accelData: ", str(accelData))
+        pub.publish(accelData)
+        rate.sleep
+
+if __name__ == '__main__':
+    try:
+        collectData()
+    except rospy.ROSInterruptException:
+        pass
+

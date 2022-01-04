8bbc79b2014e2182df98822750c78c1ac1acab68
Julius Preuschoff <julius.preuschoff@haw-hamburg.de>
2022-01-04 21:38:17 +0100

    new ESC changed stop value

--- rosNodes/scripts/cruisecontrol_node.py
+++ rosNodes/scripts/cruisecontrol_node.py
@@ -4,38 +4,12 @@ import rospy
 from std_msgs.msg import UInt16
 from std_msgs.msg import Bool
 
-startsignal = False
-
-def callbackStartsignal(data):
-    startsignal = data.data
-
-def cruiseControl():
-    # Subcribers
-    subStartsignal = rospy.Subscriber('startsignal', Bool, callbackStartsignal)
-
-    # Publishers
-    pubSpeed = rospy.Publisher('speed', UInt16, queue_size=10)
-    pubAngle = rospy.Publisher('angle', UInt16, queue_size=10)
-
-    rospy.init_node('cruiseControl') # only one init call
-    rate = rospy.Rate(1) # 1 Hz
-    speed = 90
-    angle = 90
-
-    while not rospy.is_shutdown():
-        rospy.loginfo("Speed: %s", str(speed))
-        pubSpeed.publish(speed)
-
-        rospy.loginfo("Angle: %s", str(angle))
-        pubAngle.publish(angle)
-
-        rate.sleep()
-
 class CruiseControl:
     def __init__(self):
         # Parameter
         self.rate = rospy.Rate(1) # 1 Hz
-        self.speed = 100
+        self.vehicleStop = 90 # vehicle stops
+        self.speed = 90 # init with vehicle stop value
         self.angle = 93 # small offset correction because of the tape on the wheel
         self.startsignal = False
         self.rounds = 0
@@ -48,16 +22,15 @@ class CruiseControl:
         self.pubSpeed = rospy.Publisher('speed', UInt16, queue_size=10)
         self.pubAngle = rospy.Publisher('angle', UInt16, queue_size=10)
 
-    
     def callbackStartsignal(self, data):
         self.startsignal = data.data
-        self.speed = 85
+        self.speed = 85 # slow forward
         self.pubSpeedAngle()
     
     def callbackRounds(self, data):
         self.rounds = data.data
         if self.rounds >= 50:
-            self.speed = 100
+            self.speed = self.vehicleStop
             self.pubSpeedAngle()
     
     def pubSpeedAngle(self):
